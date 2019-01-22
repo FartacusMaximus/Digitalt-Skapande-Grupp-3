6cf84e5e96ecae8f97f2e3031e03a231f8e561c1
Adam Hjelm <adam.hjelm@elev.ga.lbs.se>
2019-01-22 10:34:05 +0100

    DashPrototype

---
+++ Assets/DashScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashScript : MonoBehaviour
+{
+    public int dashSpeed;
+    private Rigidbody2D rbody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>(); 
+        dashSpeed = 15;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            rbody.velocity = new Vector2 (dashSpeed, 0);
+        }
+    }
+}
