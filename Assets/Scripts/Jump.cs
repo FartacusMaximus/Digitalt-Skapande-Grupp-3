5a274da5794f96c7e3cd162b4525705acce21993
william.arvidsson <william.arvidsson@elev.ga.lbs.se>
2019-01-29 08:45:31 +0100

    ??

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public GroundCheck GroundCheck;
+    private Rigidbody2D rbody;
+    public float jumpHeight = 10;
+    // Use this for initialization
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (GroundCheck.Touches > 0)
+            {
+                rbody.velocity = new Vector2(rbody.velocity.x, jumpHeight);
+            }
+        }
+    }
+}
