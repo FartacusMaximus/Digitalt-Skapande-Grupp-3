ae3ba2b592799678dc71550b547f3c2e6c114d2b
william.arvidsson <william.arvidsson@elev.ga.lbs.se>
2019-01-29 08:17:43 +0100

    extra files

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour {
+    public int Touches;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Touches++;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Touches--;
+    }
+}
